b8976a39cd4673518c7ab6ffbc651375a6101cf5
Pavan Kumar <59686265+Pavan164-ml@users.noreply.github.com>
2021-11-04 16:49:12 +0530

    Implemented everything but the flask app is not yet completed as there was an issue with the prediction function in app.py

--- src/utils/app.py
+++ src/utils/app.py
@@ -1,17 +1,17 @@
 from flask import Flask, render_template,request,jsonify
-import pickle
-import h5py
+import tensorflow as tf
 import numpy as np
+import logging
 
 app =Flask(__name__)
 
+FILE_PATH = "D:/Folders/Coding Stuff/Machine learning stuff/iNeuron/Projects/Perfect Implementations/ANN-Project-Modular/artifacts/model/20211031_195453_model.h5"
 # loading the trained model
+model = tf.keras.models.load_model(FILE_PATH)
+print("*"*60)
+print(model)
 
-model = 
-
-
-
-@app.route('/',methods=['POST'])
+@app.route('/')
 def home_page():
     return render_template('index.html')
 
@@ -24,11 +24,10 @@ def predict():
     # retrieving values from form
     init_features = [float(x) for x in request.form.values()]
     final_features = [np.array(init_features)]
-
+    print(final_features.shape)
     prediction = model.predict(final_features) # making prediction
 
-
-    return render_template('index.html', prediction_text='Predicted Class: {}'.format(prediction)) # rendering the predicted result
+    return render_template('after.html', data=prediction) # rendering the predicted result
 
 
 if __name__ == '__main__':
